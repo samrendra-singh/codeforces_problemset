a8cdfab96e552ea6e1ba157f56342cd7d89711e6
Samrendra Pratap Singh <samrendrapersonal@gmail.com>
2025-10-19 00:33:39 +0530

    More questions from ICPC sheet #1
    solved four questions from sheet #1.

---
+++ area_of_a_circle.cpp
@@ -0,0 +1,39 @@
+/*
+ * area_of_a_circle.cpp
+ * 
+ * Copyright 2025 Samrendra Pratap Singh <samrendrapratapsingh@Samrendras-MacBook-Air.local>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(int argc, char **argv)
+{
+	double radius, area;
+	const double PI = 3.141592653;
+	
+	cin >> radius;
+	area = PI * radius * radius;
+	cout << fixed << setprecision(9);
+	cout << area;
+	return 0;
+}
+
